116271afc632c90427453ff98a54035703dd0fdc
Laurent Jouanneau <dev@ljouanneau.com>
2022-10-20 17:26:56 +0200

    [tests] fix errors during the startup of lizmap container
    The mailer should not be configured.

--- tests/lizmap/etc/conf/profiles.d/profile.ini.php
+++ tests/lizmap/etc/conf/profiles.d/profile.ini.php
@@ -1,3 +1,18 @@
+[jdb]
+
+default=jauth
+jacl2_profile=jauth
+lizlog=jauth
+
+[jdb:jauth]
+driver=pgsql
+host=pgsql
+port=5432
+database=lizmap
+user=lizmap
+password="lizmap1234!"
+search_path=public
+
 [jdb:pgmetadata]
 
 driver=pgsql
